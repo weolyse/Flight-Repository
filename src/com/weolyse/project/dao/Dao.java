081863cb44e41844e8d4dd5294be4b573b149c93
abdunur <amangeldievabdunur@gmail.com>
2026-05-02 21:48:00 +0500

    created FlightDao

---
+++ src/com/weolyse/project/dao/Dao.java
@@ -0,0 +1,18 @@
+package com.weolyse.project.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<I, E> {
+
+    List<E> findAll();
+
+    Optional<E> findById(I id);
+
+    boolean delete(I id);
+
+    E create(E entity);
+
+    void update(E entity);
+
+}
---
+++ src/com/weolyse/project/dao/FlightDao.java
@@ -0,0 +1,79 @@
+package com.weolyse.project.dao;
+
+import com.weolyse.project.entity.Flight;
+import com.weolyse.project.entity.FlightStatus;
+import com.weolyse.project.util.ConnectionManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FlightDao implements Dao<Long, Flight> {
+
+    private static final String FIND_ALL_SQL = """
+            SELECT *
+            FROM flight
+            """;
+
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + """
+            WHERE id=?
+            """;
+
+
+    @Override
+    public List<Flight> findAll() {
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL_SQL)) {
+
+            List<Flight> flights = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                flights.add(build(resultSet));
+            }
+            return flights;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    @Override
+    public Optional<Flight> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public Flight create(Flight entity) {
+        return null;
+    }
+
+    @Override
+    public void update(Flight entity) {
+
+    }
+
+    private Flight build(ResultSet resultSet) {
+        try {
+            return new Flight(
+                    resultSet.getObject("id", Long.class),
+                    resultSet.getObject("flight_no", String.class),
+                    resultSet.getObject("departure_date", Timestamp.class).toLocalDateTime(),
+                    resultSet.getObject("departure_airport_code", String.class),
+                    resultSet.getObject("arrival_date", Timestamp.class).toLocalDateTime(),
+                    resultSet.getObject("arrival_airport_code", Timestamp.class).toString(),
+                    resultSet.getObject("aircraft_id", Integer.class),
+                    FlightStatus.valueOf(resultSet.getObject("status", String.class))
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
